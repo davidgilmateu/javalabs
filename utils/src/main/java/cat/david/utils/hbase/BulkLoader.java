b37136d675f8ec78656c78a4af0ada0c7a818749
David Gil Mateu <davidgilmateu>
2016-05-16 12:20:21 +0200

    Pill added for clean termination

--- utils/src/main/java/cat/david/utils/hbase/BulkLoader.java
+++ utils/src/main/java/cat/david/utils/hbase/BulkLoader.java
@@ -2,10 +2,12 @@ package cat.david.utils.hbase;
 
 import java.io.IOException;
 import java.io.InterruptedIOException;
+import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.client.HTable;
@@ -18,10 +20,11 @@ import org.slf4j.LoggerFactory;
 public class BulkLoader {
 
 	private BlockingQueue<KeyValue> queue;
-	public static int bufferSize = 65536;//static, yes, improvable after the platform
-	private int WAITING_TIME		= 1000;
+	private int bufferSize = 65536;
+	private int waitingTime		= 1000;
 	private ExecutorService executorService;
 	private Logger log = LoggerFactory.getLogger(BulkLoader.class);
+	private boolean ended = false;
 
 	private HTable htable;
 
@@ -36,16 +39,84 @@ public class BulkLoader {
 	}
 	
 	/**
+	 * 
+	 * @return how many entries can the queue hold
+	 */
+	public int getBufferSize() {
+		return bufferSize;
+	}
+
+	/**
+	 * 
+	 * @param bufferSize number of entries the queue can hold
+	 */
+	public void setBufferSize(int bufferSize) {
+		this.bufferSize = bufferSize;
+	}
+
+	/**
+	 * 
+	 * @return the time in milliseconds we'll wait before the next queue check
+	 */
+	public int getWaitingTime() {
+		return waitingTime;
+	}
+
+	/**
+	 * 
+	 * @param waitingTime during ending, we'll check the queue is empty 
+	 * again after waitingTime milliseconds
+	 */
+	public void setWaitingTime(int waitingTime) {
+		this.waitingTime = waitingTime;
+	}
+
+	/**
 	 * Starts a new thread that will listen through the 
 	 * {@link #put(String, String) put} method until
 	 * {@link #end end} is called
 	 * @param conf use HBaseConfiguration.create() to create it
 	 * @param table where all data will go
 	 * @param columnFamily to make it simple, one BulkLoader per family
+	 * @param columnValue where the data will lay
+	 * @param waitingTime during ending, we'll check the queue is empty 
+	 * again after waitingTime milliseconds
+	 * @param bufferSize elements in the queue
 	 * @throws IOException
+	 */
+	public BulkLoader(Configuration conf, String table, byte[] columnFamily, byte[] columnValue, Integer waitingTime, Integer bufferSize) throws IOException {
+		super();
+		if (waitingTime!=null)
+			this.waitingTime = waitingTime;
+		if (bufferSize!=null)
+			this.bufferSize = bufferSize;
+		execute(conf, table, columnFamily, columnValue);
+	}
+	
+	/**
+	 * Starts a new thread that will listen through the 
+	 * {@link #put(String, String) put} method until
+	 * {@link #end end} is called
+	 * @param conf use HBaseConfiguration.create() to create it
+	 * @param table where all data will go
+	 * @param columnFamily to make it simple, one BulkLoader per family
+	 * @param columnValue where the data will lay
+	 * @throws IOException if the object to refer to the table could not be created
 	 */
 	public BulkLoader(Configuration conf, String table, byte[] columnFamily, byte[] columnValue) throws IOException {
 		super();
+		execute(conf, table, columnFamily, columnValue);
+	}
+	
+	/**
+	 * 
+	 * @param conf hadoop conf
+	 * @param table where data goes
+	 * @param columnFamily where the column lays
+	 * @param columnValue where the data will lay
+	 * @throws IOException if the object to refer to the table could not be created
+	 */
+	public void execute(Configuration conf, String table, byte[] columnFamily, byte[] columnValue) throws IOException {
 		htable = new HTable(conf, table);
 		final byte[] cf = columnFamily;
 		final byte[] cv = columnValue;
@@ -53,26 +124,40 @@ public class BulkLoader {
 		executorService = Executors.newSingleThreadExecutor();
 		executorService.execute(new Runnable() {
 		    public void run() {
-		        while(true){
+		        while(!ended){
 		        	KeyValue keyValue;
 					try {
 						keyValue = queue.take();
-			        	Put p = new Put(keyValue.key);
-			        	byte[] value = Bytes.toBytes(keyValue.value);
-			    		p.add(cf, cv, value);
-			        	htable.put(p);
+						if(ended) break;
+						writeToHbase(keyValue, cf, cv);
 					} catch (InterruptedException e) {
+						if(ended) break;
+						log.error("Interrupted while waiting for {}", htable.getName());
 						log.error("{}", e);
-						Thread.currentThread().interrupt();
-					} catch (RetriesExhaustedWithDetailsException e) {
-						log.error("{}", e);
-					} catch (InterruptedIOException e) {
-						log.error("{}", e);
-						Thread.currentThread().interrupt();
+						return;
 					}
 		        }
 		    }
 		});
+	}
+	
+	/**
+	 * Actually does the job
+	 * @param keyValue data to be written
+	 * @param columnFamily where the column lays
+	 * @param columnValue where the data goes
+	 */
+	private void writeToHbase(KeyValue keyValue, byte[] columnFamily, byte[] columnValue){
+    	Put p = new Put(keyValue.key);
+    	byte[] value = Bytes.toBytes(keyValue.value);
+		p.add(columnFamily, columnValue, value);
+    	try {
+			htable.put(p);
+		} catch (RetriesExhaustedWithDetailsException e) {
+			log.error("{}", e);
+		} catch (InterruptedIOException e) {
+			log.error("{}", e);
+		}
 	}
 	
 	/**
@@ -85,26 +170,59 @@ public class BulkLoader {
 		try {
 			queue.put(new KeyValue(key, value));
 		} catch (InterruptedException e) {
+			log.error("Interrupted while putting in {}", htable.getName());
 			log.error("{}", e);
 		}
+	}
+	
+	/**
+	 * the queue is waiting in an eternal take. 
+	 * Let's inject a pill to unstuck it
+	 */
+	private void honorEndOfQueue(){
+		ended = true;
+		try {
+			queue.put(new KeyValue(new byte[]{},""));
+		} catch (InterruptedException e) {
+			log.error("{}", e);
+		}		
 	}
 	
 	/**
 	 * Waits until all jobs are terminated and closes all services
 	 */
 	public void end(){
-		log.info("Terminating BulkLoader with {} pending entries", queue.size());
+		int queueSize = queue.size();
+		log.info("Terminating BulkLoader with {} pending entries", queueSize);
 		while(queue.size()>0)
 			try {
-				Thread.sleep(WAITING_TIME);
+				Thread.sleep(waitingTime);
+				if(queue.size()==queueSize)//oops, we're not consuming?
+					Thread.sleep(waitingTime*9);
+				queueSize = queue.size();
+				if (queueSize>0)
+					log.info("Still {} for {}", queueSize, htable.getName());
+				else
+					log.info("The queue is clean");
 			} catch (InterruptedException e) {
 				log.error("{}", e);
 			}
-		executorService.shutdown();
 		try {
 			this.htable.close();
 		} catch (IOException e) {
 			log.error("{}", e);
 		}
+		honorEndOfQueue();
+		log.info("Table {} closed. Shutting down", htable);
+		executorService.shutdown();
+		try {
+		    if (!executorService.awaitTermination(waitingTime, TimeUnit.MILLISECONDS)) {
+		        executorService.shutdownNow();
+		    }
+		} catch (InterruptedException e) {
+		    List<Runnable> shutable = executorService.shutdownNow();
+		    log.error("Shutting down {} threads", shutable.size());
+		}
+		log.info("Service shut down");
 	}
 }
